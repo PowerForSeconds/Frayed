d65fea17ec426118da8aad38a0cd74a2dfff8be3
PowerForSeconds <powerforseconds@gmail.com>
2020-02-04 16:30:41 -0800

    Made it more hellish

--- Frayed.py
+++ Frayed.py
@@ -27,41 +27,23 @@ while n < len(lines):
         break
     line = lines[n]
     if line[0] == "var":
-        if len(line) == 3:
+        if len(line) == 2:
             try:
                 line[1] = int(line[1])
                 error("Variable name cannot be a number" + "\n" + "Line " + str(n + 1))
                 exiting = True
                 
             except:
-                try:
-                    line[2] = int(line[2])
-                    storage[line[1]] = line[2]
-                except:
-                    try:
-                        storage[line[1]] = storage[line[2]]
-                    except:
-                        error("Variable " + str(line[2]) + " does not exist" + "\n" + "Line " + str(n + 1))
-                        exiting = True
+                storage[line[1]] = 0
         else:
-            error("Var takes 2 inputs, not " + str(len(line) - 1) + "\n" + "Line " + str(n + 1))
+            error("Var takes 1 input, not " + str(len(line) - 1) + "\n" + "Line " + str(n + 1))
             exiting = True
 
     if line[0] == "add":
-        if len(line) == 3:
-            total = 0
+        if len(line) == 2:
             try:
-                line[1] = int(line[1])
-                total += line[1]
-            except:
-                try:
-                    total += storage[line[1]]
-                except:
-                    error("Variable " + str(line[1]) + " does not exist" + "\n" + "Line " + str(n + 1))
-                    exiting = True
-            try:
-                useless = storage[line[2]]
-                storage[line[2]] += total
+                useless = storage[line[1]]
+                storage[line[1]] += 1
             except:
                 error("Variable " + str(line[2]) + " does not exist" + "\n" + "Line " + str(n + 1))
                 exiting = True
@@ -70,25 +52,15 @@ while n < len(lines):
             exiting = True
 
     if line[0] == "sub":
-        if len(line) == 3:
-            total = 0
+        if len(line) == 2:
             try:
-                line[1] = int(line[1])
-                total += line[1]
+                useless = storage[line[1]]
+                storage[line[1]] -= 1
             except:
-                try:
-                    total += storage[line[1]]
-                except:
                     error("Variable " + str(line[1]) + " does not exist" + "\n" + "Line " + str(n + 1))
                     exiting = True
-            try:
-                useless = storage[line[2]]
-                storage[line[2]] -= total
-            except:
-                error("Variable " + str(line[2]) + " does not exist" + "\n" + "Line " + str(n + 1))
-                exiting = True
         else:
-            error("Add takes 2 inputs, not " + str(len(line) - 1) + "\n" + "Line " + str(n + 1))
+            error("Add takes 1 input, not " + str(len(line) - 1) + "\n" + "Line " + str(n + 1))
             exiting = True
 
     if line[0] == "goto":
